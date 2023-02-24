63f8958622d2ccb0eb111c9260e1c5136ead5fe0
sey323 <sey323_nm@yahoo.co.jp>
2023-02-24 20:57:56 +0900

    サンプルレスポンスの返却に失敗していた箇所を修正

--- app/schemas/proposal/domain.py
+++ app/schemas/proposal/domain.py
@@ -13,7 +13,7 @@ class Proposal(BaseModel):
     attachment_url: str = Field(
         "http://localhost", max_length=8192, description="提案を詳細に説明するファイル"
     )
-    target_amount: PositiveInt = Field(0, description="")
+    target_amount: PositiveInt = Field(1000, description="目標金額")
     is_recruiting_teammates: bool = Field(False, description="チームメイトを募集する場合：true　")
     other_contents: str = Field(False, max_length=8192, description="捕捉情報")
     tags: List[str] = Field(["サンプルA", "サンプルB"], description="キーワード")
