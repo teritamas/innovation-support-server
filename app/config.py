4c8c5d1e7533783cc6ca3fb67e95336630aab55d
sey323 <sey323_nm@yahoo.co.jp>
2023-02-25 01:34:06 +0900

    環境変数からパラメータを取得する設定

---
+++ app/config.py
@@ -0,0 +1,10 @@
+from os import environ
+
+from dotenv import load_dotenv
+
+load_dotenv(verbose=True)
+
+env = environ.get("ENV", "prd")
+
+cred_path = environ.get("CRED_PATH", "")
+firebase_database_url = environ.get("FIREBASE_DATABASE_URL", "")
