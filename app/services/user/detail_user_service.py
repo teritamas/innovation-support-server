fb40ebe0b8e22bde48a34ed791f5d222aa1e107a
sey323 <sey323_nm@yahoo.co.jp>
2023-02-25 12:10:52 +0900

    コントラクト実行のマーカー付与

--- app/services/user/detail_user_service.py
+++ app/services/user/detail_user_service.py
@@ -1,6 +1,10 @@
-from app.schemas.user.domain import User
 from app.facades.firebase import users_store
+from app.schemas.user.domain import User
 
 
 def execute(user_id) -> User | None:
-    return users_store.fetch_user(id=user_id)
+    user = users_store.fetch_user(id=user_id)
+
+    # TODO: ここでネットワークからウォレットの情報を取得する。
+    # wallet_address = user.wallet_address
+    return user
