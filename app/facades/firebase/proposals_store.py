ce64da03ff990be0b9cfc814fb458380d2527ce6
Ichirou.S <sey323@users.noreply.github.com>
2023-02-25 11:20:17 +0900

    ProposalAPIをデプロイ (#12)

---
+++ app/facades/firebase/proposals_store.py
@@ -0,0 +1,33 @@
+from typing import List
+
+from app.facades.firebase import fire_store
+from app.schemas.proposal.domain import Proposal
+
+COLLECTION_PREFIX = "proposals"
+
+
+def add_proposal(id: str, content: Proposal):
+    """提案内容を新規追加する
+
+    Args:
+        id (str): ProposalId
+        content (Proposal): 追加する提案情報
+    """
+    fire_store.add(collection=COLLECTION_PREFIX, id=id, content=content.dict())
+
+
+def fetch_proposal(id: str) -> Proposal:
+    """提案内容を検索する
+
+    Args:
+        id (str): ProposalId
+    """
+    return Proposal.parse_obj(
+        fire_store.fetch(collection=COLLECTION_PREFIX, id=id)
+    )
+
+
+def find_proposals() -> List[Proposal]:
+    """提案内容を全て検索する"""
+    proposals = fire_store().collection(COLLECTION_PREFIX).stream()
+    return [Proposal.parse_obj(proposal.to_dict()) for proposal in proposals]
---
+++ app/facades/gcs/gcs.py
@@ -0,0 +1,20 @@
+from app.facades.gcs import gcs
+
+
+def upload_pdf(data: bytes, destination_blob_name: str) -> str:
+    blob = gcs().blob(destination_blob_name)
+    generation_match_precondition = 0
+
+    blob.upload_from_string(
+        data,
+        content_type="application/pdf",
+        if_generation_match=generation_match_precondition,
+    )
+
+    print(f"File uploaded to {destination_blob_name}.")
+
+
+def download_pdf(destination_blob_name: str) -> bytes:
+    blob = gcs().blob(destination_blob_name)
+
+    return blob.download_as_bytes()
