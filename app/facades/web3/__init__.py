4db2d0e1da93f8ec8c00df48b5d5179c4a826fe6
Ichirou.S <sey323@users.noreply.github.com>
2023-02-25 18:31:47 +0900

    Web3回りの設定をconfigから取得するように変更 (#16)

--- app/facades/web3/__init__.py
+++ app/facades/web3/__init__.py
@@ -1,12 +1,10 @@
+from app import config
 from app.facades.web3.account import ContractOwner
 from app.facades.web3.smart_contracts import ProposalNFT
 
-# サンプル用のコントラクト
-SAMPLE_CONTRACT_ADDRESS = "0x99802244430b37362Eb494ec5Ce92fBC8F38f36C"
-
+# 提案用NFT
 proposal_nft = ProposalNFT(
-    contract_owner=ContractOwner("./key/dev_private.key"),
-    # provider_network_url="https://evm.shibuya.astar.network",
-    provider_network_url="https://goerli.blockpi.network/v1/rpc/public",
-    contract_address=SAMPLE_CONTRACT_ADDRESS,
+    contract_owner=ContractOwner(config.system_wallet_private_key_path),
+    provider_network_url=config.provider_network,
+    contract_address=config.proposal_nft_contract_address,
 )
