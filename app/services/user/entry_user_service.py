1d37b5d8d67ebc3803ea569c6d5e96cd9fa0c2f6
Ichirou.S <sey323@users.noreply.github.com>
2023-02-26 00:02:03 +0900

    wallet_addressでユーザ検索のAPI追加 (#20)

--- app/services/user/entry_user_service.py
+++ app/services/user/entry_user_service.py
@@ -7,7 +7,7 @@ from app.utils.common import generate_id_str
 def execute(request: EntryUserRequest) -> str:
     users = users_store.fetch_user_from_wallet_address(request.wallet_address)
 
-    if users != []:  # 存在する場合はそのユーザIDを返す
+    if users == []:  # 存在する場合はそのユーザIDを返す
         return users[0].user_id
 
     # 存在しない場合は新規に作成する。
