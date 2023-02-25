605c3f4a84f728f3e7daf90d1aeb6962572665d7
Ichirou.S <sey323@users.noreply.github.com>
2023-02-25 18:33:06 +0900

    動作確認のマージ漏れ (#17)

--- app/config.py
+++ app/config.py
@@ -10,3 +10,11 @@ cred_path = environ.get("CRED_PATH", "")
 google_cloud_storage_bucket_name = environ.get(
     "GOOGLE_CLOUD_STORAGE_BUCKET_NAME", ""
 )
+
+provider_network = environ.get("PROVIDE_NETWORK", "")
+proposal_nft_contract_address = environ.get(
+    "PROPOSAL_NFT_CONTRACT_ADDRESS", ""
+)
+system_wallet_private_key_path = environ.get(
+    "SYSTEM_WALLET_PRIVATE_KEY_PATH", ""
+)
