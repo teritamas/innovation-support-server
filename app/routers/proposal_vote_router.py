e903ccf49efeef9186fe1a348522ac2bedc31280
Ichirou.S <sey323@users.noreply.github.com>
2023-02-28 00:40:27 +0900

    ユーザの提案に対する投票状態を返すAPIの作成 (#42)

--- app/routers/proposal_vote_router.py
+++ app/routers/proposal_vote_router.py
@@ -1,8 +1,15 @@
 from fastapi import APIRouter, HTTPException, status
 
+from app.schemas.proposal_vote.domain import ProposalVote
 from app.schemas.proposal_vote.requests import EntryProposalVoteRequest
-from app.schemas.proposal_vote.responses import EntryProposalVoteResponse
-from app.services.proposal_vote import entry_proposal_vote_service
+from app.schemas.proposal_vote.responses import (
+    EntryProposalVoteResponse,
+    FetchProposalVoteResponse,
+)
+from app.services.proposal_vote import (
+    entry_proposal_vote_service,
+    fetch_proposal_vote_service,
+)
 
 proposal_vote_router = APIRouter(prefix="/proposal", tags=["vote"])
 
@@ -20,3 +27,16 @@ def entry_proposal_vote(proposal_id: str, request: EntryProposalVoteRequest):
     return EntryProposalVoteResponse(
         vote_nft_token_id=vote_nft_token_id, reward=10.0
     )
+
+
+@proposal_vote_router.get(
+    "/{proposal_id}/vote/{vote_user_id}",
+    response_model=FetchProposalVoteResponse,
+)
+def fetch_proposal_vote(proposal_id: str, vote_user_id: str):
+    response: ProposalVote | None = fetch_proposal_vote_service.execute(
+        proposal_id, vote_user_id
+    )
+    if response is None:
+        return FetchProposalVoteResponse(voted=False, vote_content=None)
+    return FetchProposalVoteResponse(voted=True, vote_content=response)
