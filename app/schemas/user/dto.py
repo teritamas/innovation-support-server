4e2bc4ada719a8c52e4b7dab9742b3e3797d6e4c
Ichirou.S <sey323@users.noreply.github.com>
2023-03-13 23:28:51 +0900

    FEの修正に合わせた更新 (#104)

--- app/schemas/user/dto.py
+++ app/schemas/user/dto.py
@@ -1,14 +1,19 @@
 from typing import List
 
-from pydantic import BaseModel, Field
+from pydantic import Field
 
 from app.schemas.proposal.domain import Proposal
 from app.schemas.proposal_vote.domain import ProposalVote
 from app.schemas.user.domain import User
 
 
+class UserProposalVote(ProposalVote):
+    proposal_id: str = Field("abcdefg", max_length=256, description="提案ID")
+    title: str = Field("タイトルサンプル", max_length=256, description="提案のタイトル")
+
+
 class DetailUserDto(User):
     proposals: List[Proposal] = Field([Proposal()], description="ユーザが実施した提案一覧")
-    proposal_votes: List[ProposalVote] = Field(
-        [ProposalVote()], description="ユーザが実施した投票一覧"
+    proposal_votes: List[UserProposalVote] = Field(
+        [], description="ユーザが実施した投票一覧"
     )
