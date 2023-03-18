c3d6193f5fc296c3b8b203389078542f02fbb075
sey323 <sey323_nm@yahoo.co.jp>
2023-03-18 19:28:01 +0900

    サンプル挿入用のコードの作成と単体テストの修正

--- tests/test_prize_router.py
+++ tests/test_prize_router.py
@@ -1,3 +1,4 @@
+import pytest
 from fastapi.testclient import TestClient
 
 from app.facades.database import prizes_store, users_store
@@ -36,6 +37,76 @@ def test_entry_prize(mocker):
     actual = EntryPrizeResponse.parse_obj(response.json())
 
     assert actual.prize_id == test_prize_id
+
+
+@pytest.mark.skipif(True, reason="サンプルテスト登録用のテストコードのため")
+def test_entry_prize_for_sample(mocker):
+    """研修の景品を登録できること"""
+    test_signup_not_exists(mocker)
+    # give
+    test_user_id = "test_user_id"
+
+    response = _insert_request(
+        test_user_id,
+        name="プロジェクトマネジメント入門: 企画から納品までの基礎知識",
+        description="プロジェクトマネジメントの基礎知識を学び、プロジェクトの企画、実行、納品までのプロセスを理解する研修。",
+        required_token_amount=10,
+        recommendation_score=3,
+    )
+    response = _insert_request(
+        test_user_id,
+        name="ビジネスプレゼンテーション: 魅力的なプレゼン資料の作成とスピーチの技法",
+        description="効果的なプレゼンテーションの作成方法と、プレゼンテーションの際に必要なスキルを学ぶ研修。",
+        required_token_amount=5,
+        recommendation_score=4,
+    )
+    response = _insert_request(
+        test_user_id,
+        name="マーケティング戦略: 顧客との接点を最大限に活用する方法",
+        description="マーケティング戦略の基礎知識と、顧客との接点を最大限に活用するための戦略を学ぶ研修。",
+        required_token_amount=10,
+        recommendation_score=4,
+    )
+
+    response = _insert_request(
+        test_user_id,
+        name="データ分析入門: ExcelとGoogleスプレッドシートを使ったデータの分析方法",
+        description="ExcelやGoogleスプレッドシートを使ったデータの分析方法を学び、ビジネス上でデータを活用するためのスキルを身につける研修。",
+        required_token_amount=10,
+        recommendation_score=5,
+    )
+    response = _insert_request(
+        test_user_id,
+        name="コミュニケーションスキル: 効果的なコミュニケーションの基礎",
+        description="コミュニケーションスキルの基礎知識を学び、ビジネス上でのコミュニケーション能力を向上させるための研修。",
+        required_token_amount=12,
+        recommendation_score=5,
+    )
+
+    assert response.status_code == 200
+    actual = EntryPrizeResponse.parse_obj(response.json())
+
+
+def _insert_request(
+    test_user_id,
+    name,
+    description,
+    required_token_amount,
+    recommendation_score,
+):
+    request = EntryPrizeRequest(
+        name=name,
+        description=description,
+        required_token_amount=required_token_amount,
+        recommendation_score=recommendation_score,
+        type="Training",
+    )
+
+    response = client.post(
+        f"/prize", headers={"Authorization": test_user_id}, data=request.json()
+    )
+
+    return response
 
 
 def test_entry_prize_welfare(mocker):
@@ -79,10 +150,16 @@ def test_find_prize(mocker):
     assert response.status_code == 200
     actual = response.json().get("prizes")
     assert type(actual) == list
-    assert actual[0].get("name") == "[テスト]研修A"
-    assert actual[0].get("type") == "Training"
-    assert actual[1].get("name") == "[テスト]休息A"
-    assert actual[1].get("type") == "Welfare"
+    training = [
+        a for a in actual if a.get("prize_id") == "test_training_prize_id"
+    ][0]
+    assert training.get("name") == "[テスト]研修A"
+    assert training.get("type") == "Training"
+    welfare = [
+        a for a in actual if a.get("prize_id") == "test_welfare_prize_id"
+    ][0]
+    assert welfare.get("name") == "[テスト]休息A"
+    assert welfare.get("type") == "Welfare"
 
 
 def test_fetch_prize(mocker):
